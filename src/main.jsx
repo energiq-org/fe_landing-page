04648ec23ece2e51186e9927c8c2866e15eb2017
Khaled Fahmy <khaledfahmy345@gmail.com>
2025-04-30 17:18:38 +0300

    fix: gh-pages branch wasn't created on org-origin

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <BrowserRouter basename='https://energiq-org.github.io/fe_landing-page/'> 
+   <BrowserRouter basename='/fe_landing-page/'> 
       <App />
     </BrowserRouter>
   </StrictMode>,
