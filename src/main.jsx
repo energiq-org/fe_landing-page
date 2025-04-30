132a888f03b70d39132180cfc3367a7f657da1da
Khaled Fahmy <khaledfahmy345@gmail.com>
2025-04-30 17:11:04 +0300

    fix: url reset

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <BrowserRouter basename='./'> 
+   <BrowserRouter basename='https://energiq-org.github.io/fe_landing-page/'> 
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "https://energiq-org.github.io/fe_landing-page/",
   plugins: [react()],
 })
