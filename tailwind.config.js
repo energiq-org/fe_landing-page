c679511a0941149b4c982e274af5d1c88c5a9e33
Nourhan Hesham <117953315+nourhanHesham77@users.noreply.github.com>
2025-04-30 18:07:55 +0300

    fix width

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
         md: '768px',
         lg: '1024px',
         xl: '1536px',
-        '2xl': '1920px',
+        '2xl': '1848px',
       },
       fontFamily: {
         bebas: ['"Bebas Neue"', 'sans-serif'],
